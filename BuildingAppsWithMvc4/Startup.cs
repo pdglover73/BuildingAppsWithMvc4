763535af278ad42827455ed2844e2f24a76a0a91
Paul Glover <pdglover@hotmail.com>
2017-02-19 21:15:50 +0000

    Initial commit

---
+++ BuildingAppsWithMvc4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildingAppsWithMvc4.Startup))]
+namespace BuildingAppsWithMvc4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
