1146a9f4c322f6bd6f8903d6e7c89d0e88ba418b
Paul Glover <pdglover@hotmail.com>
2017-03-28 07:47:39 +0100

    Initial commit

---
+++ BuildingAppsWithMvc4/Controllers/CuisineController.cs
@@ -0,0 +1,21 @@
+﻿using BuildingAppsWithMvc4.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingAppsWithMvc4.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        //[Authorize]
+        [Log]
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+    }
+}
